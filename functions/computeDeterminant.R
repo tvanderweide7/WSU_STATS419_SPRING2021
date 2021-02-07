2f0e2eacd097fc35ff0e165d965fa9080bc0f1cb
tvanderweide7 <57298359+tvanderweide7@users.noreply.github.com>
2021-02-06 16:56:58 -0800

    function-computeDeterminant.R

--- functions/computeDeterminant.R
+++ functions/computeDeterminant.R
@@ -1,6 +1,7 @@
 # http://sanfoundry.com/c-program-compute-determinant-matrix/
 determinant = function(mrx){
-  mtrx = matrix(data=mrx, nrow = 3, ncol = 3)
+  mtrx = matrix(data=mrx, nrow = 3, ncol = 3, byrow = TRUE)
+  print(mtrx)
   det =    mtrx[1,1] * ((mtrx[2,2]*mtrx[3,3]) - (mtrx[3,2]*mtrx[2,3])) -mtrx[1,2] * (mtrx[2,1]
     * mtrx[3,3] - mtrx[3,1] * mtrx[2,3]) + mtrx[1,3] * (mtrx[2,1] * mtrx[3,2] - mtrx[3,1] * mtrx[2,2]);
   return(det)
