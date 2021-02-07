595bd02cfc9e5752de0904f2385057468bfbef63
tvanderweide7 <57298359+tvanderweide7@users.noreply.github.com>
2021-02-06 17:40:07 -0800

    function-computeDeterminant.R

--- functions/computeDeterminant.R
+++ functions/computeDeterminant.R
@@ -1,8 +1,14 @@
-# http://sanfoundry.com/c-program-compute-determinant-matrix/
+#inspiration came from: https://www.w3resource.com/c-programming-exercises/array/c-array-exercise-28.php
 determinant = function(mrx){
-  mtrx = matrix(data=mrx, nrow = 3, ncol = 3, byrow = TRUE)
-  print(mtrx)
-  det =    mtrx[1,1] * ((mtrx[2,2]*mtrx[3,3]) - (mtrx[3,2]*mtrx[2,3])) -mtrx[1,2] * (mtrx[2,1]
-    * mtrx[3,3] - mtrx[3,1] * mtrx[2,3]) + mtrx[1,3] * (mtrx[2,1] * mtrx[3,2] - mtrx[3,1] * mtrx[2,2]);
+  mtrx = matrix(data = mrx, nrow = 3, ncol = 3)
+  det = 0
+  for(i in 1:3){
+    twoOver = ((i + 1) %%3) 
+    threeOver = ((i + 2) %%3) 
+    if(twoOver ==0) twoOver = 3 
+    if(threeOver == 0) threeOver = 3
+    det = det + (mtrx[1,i]*(mtrx[2,twoOver] * 
+      mtrx[3,threeOver] - mtrx[2,threeOver] * mtrx[3,twoOver]));
+  }
   return(det)
 }
